c86934c73e2124c4d520b2ecef18059fa49270b5
S3700 <sjgloogman@gmail.com>
2025-01-30 17:41:13 +0100

    removed bruteforce.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from code.algorithms.bruteforce import Bruteforce
 from code.algorithms.random import RandomSolution
 from code.algorithms.hillclimber import HillClimber
 from code.algorithms.simannealing import SimulatedAnnealing
-from code.algorithms.breadthfirst import BreadthFirstSearch
-from code.algorithms.depthfirst import DepthFirstSearch
+from code.algorithms.breadthfirst import BreadthFirst
+from code.algorithms.depthfirst import DepthFirst
 from code.visualisation.visualise import print_visual
 from code.classes.experiment import TimedExperiment
 import matplotlib.pyplot as plt
@@ -11,8 +10,8 @@ import matplotlib.pyplot as plt
 def main():
     #---------------------------------Choose your protein, algorithm and runtime-------------------------------------#
     proteins = ["HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"] # Max 50 characters
-    algorithm =  SimulatedAnnealing
-    max_runtime = 30 # in seconds
+    algorithm =  RandomSolution # Choose from RandomSolution, HillClimber, SimulatedAnnealing, BreadthFirst, DepthFirst
+    max_runtime = 10 # in seconds
     #----------------------------------------------------------------------------------------------------------------#
 
     # Initialize experiment handler
@@ -31,12 +30,12 @@ def main():
         # Visualize the best folding from the experiment
         experiment_solver = experiment.algorithm(protein)
         experiment_solver.graph.apply_folding(best_folding)
-        print_visual(protein, best_folding, experiment_solver.graph)
+        print_visual(protein, best_folding, experiment_solver.graph, algorithm)
 
         # Create histogram of all scores from the experiment
         plt.figure(figsize=(10, 6))
         plt.hist(all_scores, bins=min(50, len(set(all_scores)) or 1))
-        plt.title(f'Distribution of Scores for {protein}\nBest Score: {best_score}')
+        plt.title(f'Distribution of Scores for: {protein}\nBest Score: {best_score}\nAlgorithm: {algorithm.__name__}')
         plt.xlabel('Score')
         plt.ylabel('Frequency')
         plt.grid(True)
